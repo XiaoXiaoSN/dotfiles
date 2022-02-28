7e2e185a9d52d0f0d5898e3ce6c2acd19ffe8f10
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2022-02-28 22:04:37 +0800

    feat(macOS): complete macOS brew install list

--- macos/brew.sh
+++ macos/brew.sh
@@ -3,30 +3,62 @@
 # Install command-line tools using Homebrew.
 # Install steps: https://brew.sh/
 
+# Turn off analytics https://docs.brew.sh/Analytics
+brew analytics off
+
 # Make sure we’re using the latest Homebrew.
 brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install necessary casks (GUI Application)
+# Install necessary casks (GUI Applications)
+echo "🟢 Install necessary casks (GUI Applications)"
+brew install --cask \
+	1password \
+	brave-browser \
+	iterm2
 
-# Install more recent versions of some macOS tools.
-brew install
-  openssh \
-  curl
+# Install necessary packages binaries
+echo "🟢 Install necessary packages binaries"
+brew install \
+	fish \
+	htop \
+	neovim \
+	tmux
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
+# Install other important GUI Applications
+echo "🟢 Install other important GUI Applications"
+brew install --cask \
+	docker \
+  go \
+	google-chrome \
+	itsycal \
+	notion \
+	slack \
+	openvpn-connect \ # or tunnelblick
+	visual-studio-code
 
 # Install other useful binaries.
+echo "🟢 Install other useful binaries"
 brew install \
+  awscli \
+	kind \
+	kubectx \
+  gnupg \
+	ngrok/ngrok/ngrok \
   nmap \
   tree \
   ripgrep
 
+# Install more recent versions of some macOS tools.
+echo "🟢 Install more recent versions of some macOS tools"
+brew install curl
+
 # Install font tools.
+echo "🟢 Install font tools"
 brew tap homebrew/cask-fonts
 brew install font-hack
 
 # Remove outdated versions from the cellar.
+echo "🟢 Remove outdated versions from the cellar"
 brew cleanup
