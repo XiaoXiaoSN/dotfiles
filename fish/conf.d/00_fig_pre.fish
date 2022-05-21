62a0eeaf4cd0016850f17657c42a276b3c2372c1
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2022-05-21 13:38:04 +0800

    feat(fish): add `fig` for macOS

--- fish/conf.d/00_fig_pre.fish
+++ fish/conf.d/00_fig_pre.fish
@@ -1,5 +1,1 @@
-# Fig pre block. Keep at the top of this file.
-if type -q fig
-  set -Ua fish_user_paths $HOME/.local/bin
-  eval (fig init fish pre | string split0)
-end
+eval (~/.local/bin/fig init fish pre --rcfile 00_fig_pre | string split0)
--- fish/conf.d/99_fig_post.fish
+++ fish/conf.d/99_fig_post.fish
@@ -1,4 +1,1 @@
-# Fig post block. Keep at the bottom of this file.
-if type -q fig
-  eval (fig init fish post | string split0)
-end
+eval (~/.local/bin/fig init fish post --rcfile 99_fig_post | string split0)
