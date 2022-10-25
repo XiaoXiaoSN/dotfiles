6821fca86b9d22fe03f1f0f35b56bfeac44e29e0
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2022-10-26 00:49:39 +0800

    feat(fish): update fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-export LANG=en_US.UTF-8
+set -x LANG en_US.UTF-8
 
 # Aliases
 source $HOME/.config/fish/aliases.fish
@@ -7,3 +7,8 @@ source $HOME/.config/fish/aliases.fish
 if test -e $HOME/fish-profile.fish
   source $HOME/fish-profile.fish
 end
+
+# set fish colors
+# run `set_color --print-colors` to get default colors
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
