16a20d242a715355c3df0cc85c84afdfa0980e53
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2023-07-06 21:38:04 +0800

    feat(fish): add krew plugin manager

--- fish/conf.d/kube.fish
+++ fish/conf.d/kube.fish
@@ -6,3 +6,8 @@ if type -q kubectl
       export KUBECONFIG=$HOME/.kube/config(find $HOME/.kube -iname "*.config" -or -iname "*.conf" -type f 2>/dev/null | xargs -I % echo -n ":%")
     end
 end
+
+# krew is the kubectl plugin manager: https://krew.sigs.k8s.io/
+if test -d "$HOME/.krew/bin"
+  fish_add_path $HOME/.krew/bin
+end
