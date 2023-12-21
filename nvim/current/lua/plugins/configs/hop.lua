9b975ff2823f4e6007b0da7c2c1ca0b0b86ae5ca
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2023-12-22 01:42:40 +0800

    style(nvim/current): format lua code

--- nvim/current/lua/plugins/configs/hop.lua
+++ nvim/current/lua/plugins/configs/hop.lua
@@ -1,11 +1,11 @@
-local present, hop = pcall(require, "hop")
+local present, hop = pcall(require, 'hop')
 if not present then
-  print("Missing plugin `smoka7/hop.nvim`")
+  print('Missing plugin `smoka7/hop.nvim`')
   return
 end
 
 hop.setup({
-  keys = "abcdefghijklmnopqrstuvwxyz",
+  keys = 'abcdefghijklmnopqrstuvwxyz',
 })
 
 ----------------------------------------
@@ -13,19 +13,34 @@ hop.setup({
 ----------------------------------------
 
 -- https://github.com/smoka7/hop.nvim/wiki/Commands
-local directions = require("hop.hint").HintDirection
+local directions = require('hop.hint').HintDirection
 
 -- override default `f` mappings
-vim.keymap.set("", "f", function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
+-- stylua: ignore
+vim.keymap.set(
+  "", "f",
+  function()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+  end,
+  { remap = true }
+)
 
 -- override default `F` mappings
-vim.keymap.set("", "F", function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, { remap = true })
+-- stylua: ignore
+vim.keymap.set(
+  "", "F",
+  function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+  end,
+  { remap = true }
+)
 
 -- hop to any char in the current buffer
-vim.keymap.set("n", "<leader><leader>", function()
-  hop.hint_char1({})
-end, { remap = true })
+-- stylua: ignore
+vim.keymap.set(
+  'n', '<leader><leader>',
+  function()
+    hop.hint_char1({})
+  end,
+  { remap = true }
+)
