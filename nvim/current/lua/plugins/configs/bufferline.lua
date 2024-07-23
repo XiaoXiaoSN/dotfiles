d5d6e8aeca22fcfcd424208a868a0499da1c84ee
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2024-07-23 16:24:40 +0800

    feat(nvim/current): change the close icon for buffer line

--- nvim/current/lua/plugins/configs/bufferline.lua
+++ nvim/current/lua/plugins/configs/bufferline.lua
@@ -8,9 +8,9 @@ end
 local options = {
   options = {
     offsets = { { filetype = 'NvimTree', text = '', padding = 1 } },
-    buffer_close_icon = '',
+    buffer_close_icon = '×',
     modified_icon = '',
-    close_icon = '',
+    close_icon = '×',
     show_close_icon = false,
     left_trunc_marker = ' ',
     right_trunc_marker = ' ',
