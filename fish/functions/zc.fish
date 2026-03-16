7c6c4a1b04e34de0b2068873fab61e25eb6f8570
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2026-03-16 14:05:06 +0800

    feat(fish): replace z with zoxide

--- fish/functions/zc.fish
+++ fish/functions/zc.fish
@@ -1,13 +1,27 @@
-function zc -d 'Use Editor to open directory which best matches'
-    set editor "cursor"
+function zc -d 'Use editor to open best-matching directory (zoxide first)'
+    set editor code
 
-    z -e $argv[1]
-    if [ $status -eq 0 ]
-        set dir (z -e $argv[1])
-        if [ "$editor" = "cursor" ]
-            cursor $dir
-        else
-            code $dir
-        end
+    if type -q zoxide
+        set dir (zoxide query $argv[1] 2>/dev/null)
+    else if type -q z
+        set dir (z -e $argv[1] 2>/dev/null)
+    else
+        echo "Neither zoxide nor z is installed"
+        return 1
+    end
+
+    if test -z "$dir"
+        return 1
+    end
+
+    switch $editor
+        case antigravity
+            antigravity "$dir"
+        case code
+            code "$dir"
+        case cursor
+            cursor "$dir"
+        case '*'
+            $editor "$dir"
     end
 end
--- fish/functions/zv.fish
+++ fish/functions/zv.fish
@@ -1,6 +1,18 @@
-function zv -d 'use NeoVim open directory which best matching'
-  z -e $argv[1]
-  if [ $status -eq 0 ]
-    nvim (z -e $argv[1])
-  end
+function zv -d 'use Neovim to open best-matching directory (zoxide first)'
+    if type -q zoxide
+        if test (count $argv) -eq 0
+            set dir (zoxide query -i)
+        else
+            set dir (zoxide query $argv[1] 2>/dev/null)
+        end
+    else if type -q z
+        set dir (z -e $argv[1] 2>/dev/null)
+    else
+        echo "Neither zoxide nor z is installed"
+        return 1
+    end
+
+    if test -n "$dir"
+        nvim "$dir"
+    end
 end
